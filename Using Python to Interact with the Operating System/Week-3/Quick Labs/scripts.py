4aed309226a36e895fcddd15af414c5e2dfebc81
ANKIT NIRANJAN <43110505+ankitniranjan@users.noreply.github.com>
2020-09-22 07:45:44 +0530

    Update scripts.py

--- Using Python to Interact with the Operating System/Week-3/Quick Labs/scripts.py
+++ Using Python to Interact with the Operating System/Week-3/Quick Labs/scripts.py
@@ -21,8 +21,8 @@ def replace_domain(address, old_domain, new_domain):
 def main():
   """Processes the list of emails, replacing any instances of the old domain with the new domain."""
   old_domain, new_domain = 'abc.edu', 'xyz.edu'
-  csv_file_location = '<csv_file_location>'
-  report_file = '<path_to_home_directory>' + '/updated_user_emails.csv'
+  csv_file_location = '/home/student-02-ee3a3fe22757/data/user_emails.csv'
+  report_file = '/home/student-02-ee3a3fe22757/data' + '/updated_user_emails.csv'
   user_email_list = []
   old_domain_email_list = []
   new_domain_email_list = []
