3b340142523afe83ef1df5a3e6bc2a08ced2d09b
ANKIT NIRANJAN <43110505+ankitniranjan@users.noreply.github.com>
2020-11-19 10:23:05 +0530

    Create run.py

---
+++ Automating Real-World Tasks With Python/Week-2/run.py
@@ -0,0 +1,44 @@
+# Run these commands before running python file
+
+sudo apt-get update
+sudo apt install python-django-common
+sudo systemctl start google-startup-scripts.service
+
+############################run.py###########################################################
+
+#!/usr/bin/env python3
+
+import os
+import requests
+
+# Path to the data
+path = "/data/feedback/"
+
+keys = ["title", "name", "date", "feedback"]
+
+folder = os.listdir(path)
+
+for file in folder:
+	keycount = 0
+	fb = {}
+	with open(path + file) as fl:
+		for line in fl:
+			value = line.strip()
+			fb[keys[keycount]] = value
+			keycount += 1
+	print(fb)
+	response = requests.post("http://<corpweb external IP address>/feedback/", json=fb)
+
+print(response.request.body)
+print(response.status_code)
+
+
+##########Output#####################
+
+{'name': 'John', 'date': '2017-12-21', 'feedback': 'The customer service here is very good. They helped me find a 2017 Camry with good condition in reasonable price. Campared to other dealers, they provided the lowest price. Definttely recommend!', 'title': 'Great Customer Service'}
+{'name': 'Anonymous', 'date': '2018-09-21', 'feedback': "I came in around 6pm and they seemed about to close the store. One of the sales seemed not being patient with me and made me feel like I have to either buy a car or come back later. Of course I didn't buy a car there. Hopefully they can treat every customer with more patience.", 'title': 'Waste of my time'}
+{'name': 'Anonymous', 'date': '2018-04-17', 'feedback': 'Called them to look for a second-hand RAV4 and they are very nice and patience to help me find me a few matches then scheduled an appointmet with me. Came in and they had everything ready for me. I was surprised how professional those sales are and they explained and answered all my questions. Ended up buying the car and been using it for more than a month now. Everything looks good!', 'title': 'Good deal for a 2015 RAV4'}
+{'name': 'Tom', 'date': '2019-06-05', 'feedback': "I've being look around for a second handed Lexus RX for my family and this store happened to have a few of those. The experience was similar to most car dealers. The one I ended up buying has good condition and low mileage. I am pretty satisfied with the price they offered.", 'title': 'You will find what you want here'}
+{'name': 'Katie', 'date': '2019-12-09', 'feedback': "My friend recommended this place and  I decided to give a try. I called them beforehand and made sure they have the models I was looking for. They evenly sent me a bunch of photos and specs of the car I was looking for. One the day I visited, the sales person John helped me with test drive and showed me what I should be aware of when buying a used car. I felt like I learned a lot. They made the process so smooth that I saved a lot of time. It's the best car purchasing experience I had so far!", 'title': 'Best experience so far'}
+b'{"name": "Katie", "date": "2019-12-09", "feedback": "My friend recommended this place and  I decided to give a try. I called them beforehand and made sure they have the models I was looking for. They evenly sent me a bunch of photos and specs of the car I was looking for. One the day I visited, the sales person John helped me with test drive and showed me what I should be aware of when buying a used car. I felt like I learned a lot. They made the process so smooth that I saved a lot of time. It\'s the best car purchasing experience I had so far!", "title": "Best experience so far"}'
+201
